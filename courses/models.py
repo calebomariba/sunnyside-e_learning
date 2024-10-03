309c54a63c7367a92b8124f0381bdf02e82f3211
caleb <calebomariba1993@gmail.com>
2024-10-03 21:07:47 +0300

    Add custom OrderField in fields.py and create migrations

--- courses/models.py
+++ courses/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from .fields import OrderField
+
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
