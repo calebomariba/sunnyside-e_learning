d17a6529867716a1cafce31b4fcc801e9717f31e
caleb <calebomariba1993@gmail.com>
2024-10-09 11:46:53 +0300

    Add homepage

--- e_learning/urls.py
+++ e_learning/urls.py
@@ -19,9 +19,11 @@ from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+
 
 urlpatterns = [
+    path('', include('courses.urls')),
     path("admin/", admin.site.urls),
     path(
         'accounts/login/', auth_views.LoginView.as_view(), name='login'
