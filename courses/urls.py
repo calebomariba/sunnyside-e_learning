bc554e23338113e9d061acdbf30b0813de6188fa
caleb <calebomariba1993@gmail.com>
2024-10-21 16:43:10 +0300

    Add URL patterns for filtering courses by subject and course details

--- courses/urls.py
+++ courses/urls.py
@@ -58,5 +58,16 @@ urlpatterns = [
         views.ContentOrderView.as_view(),
         name='content_order',
     ),
+        path(
+        'subject/<slug:subject>/',
+        views.CourseListView.as_view(),
+        name='course_list_subject',
+    ),
+    path(
+        '<slug:slug>/',
+        views.CourseDetailView.as_view(),
+        name='course_detail',
+    ),
+
 ]
 
