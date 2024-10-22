c8778be4a8e376ecd565fd83c39cd5a9a6f018f4
caleb <calebomariba1993@gmail.com>
2024-10-22 13:03:38 +0300

    Add render_to_string() function for rendering a template and returning the rendered content as a string.

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,7 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
 from .fields import OrderField
+from django.template.loader import render_to_string
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -96,6 +97,12 @@ class ItemBase(models.Model):
 
     def __str__(self):
         return self.title
+
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self},
+        )
 
 
 class Text(ItemBase):
