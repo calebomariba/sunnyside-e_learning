55eba0c6203c7f2cbf48c94649b80973a9899624
caleb <calebomariba1993@gmail.com>
2024-10-22 11:14:00 +0300

    Add StudentCourseListView to see courses that students are enrolled in.

--- students/views.py
+++ students/views.py
@@ -6,6 +6,8 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
 from .forms import CourseEnrollForm
+from django.views.generic.list import ListView
+from courses.models import Course
 
 
 
@@ -37,3 +39,12 @@ class StudentEnrollCourseView(LoginRequiredMixin, FormView):
         return reverse_lazy(
             'student_course_detail', args=[self.course.id]
         )
+
+
+class StudentCourseListView(LoginRequiredMixin, ListView):
+    model = Course
+    template_name = 'students/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(students__in=[self.request.user])
