e36a8cdfd43a12db74970802f2a55f388d6b309a
caleb <calebomariba1993@gmail.com>
2024-10-25 11:36:33 +0300

    convert the SubjectListView and SubjectDetailView views into a single ViewSet

--- courses/api/views.py
+++ courses/api/views.py
@@ -8,15 +8,20 @@ from courses.models import Course, Subject
 
 
 
-class SubjectListView(generics.ListAPIView):
+# class SubjectListView(generics.ListAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+#     pagination_class = StandardPagination
+
+
+# class SubjectDetailView(generics.RetrieveAPIView):
+#     queryset = Subject.objects.annotate(total_courses=Count('courses'))
+#     serializer_class = SubjectSerializer
+
+class SubjectViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Subject.objects.annotate(total_courses=Count('courses'))
     serializer_class = SubjectSerializer
     pagination_class = StandardPagination
-
-
-class SubjectDetailView(generics.RetrieveAPIView):
-    queryset = Subject.objects.annotate(total_courses=Count('courses'))
-    serializer_class = SubjectSerializer
 
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.prefetch_related('modules')
