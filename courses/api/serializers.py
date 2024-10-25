15aaf8dec1a4bf3c7190c7bc269f0bfc55d88c76
caleb <calebomariba1993@gmail.com>
2024-10-25 11:08:50 +0300

    Create nested seriazers for modules

--- courses/api/serializers.py
+++ courses/api/serializers.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import serializers
-from courses.models import Course, Subject
+from courses.models import Course, Module, Subject
 
 class SubjectSerializer(serializers.ModelSerializer):
     total_courses = serializers.IntegerField()
@@ -19,13 +19,13 @@ class SubjectSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'slug', 'total_courses', 'popular_courses']
 
 
-# class ModuleSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Module
-#         fields = ['order', 'title', 'description']
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Module
+        fields = ['order', 'title', 'description']
 
 class CourseSerializer(serializers.ModelSerializer):
-    # modules = ModuleSerializer(many=True, read_only=True)
+    modules = ModuleSerializer(many=True, read_only=True)
 
     class Meta:
         model = Course
