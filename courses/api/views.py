8fd83d54c852c5dde12ed99af9d45ac36dd558b3
caleb <calebomariba1993@gmail.com>
2024-10-25 15:24:23 +0300

    add a permission_classes attribute to CourseEnrollView

--- courses/api/views.py
+++ courses/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from courses.api.serializers import  CourseSerializer, SubjectSerializer
 from courses.api.pagination import StandardPagination
 from courses.models import Course, Subject
@@ -34,7 +35,8 @@ class CourseViewSet(viewsets.ReadOnlyModelViewSet):
 
 class CourseEnrollView(APIView):
     authentication_classes = [BasicAuthentication]
-    
+    permission_classes = [IsAuthenticated]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
