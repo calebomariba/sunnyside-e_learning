ed46716ae52619b83d1a0e4d1ea4cc4e31e97e09
caleb <calebomariba1993@gmail.com>
2024-10-28 19:05:17 +0300

    Update settings.py channel _layers

--- e_learning/settings.py
+++ e_learning/settings.py
@@ -164,3 +164,12 @@ REST_FRAMEWORK = {
 }
 
 ASGI_APPLICATION = 'e_learning.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
