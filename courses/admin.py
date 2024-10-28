4f406d1a2b53d8ae6895bf80d56e0e7e6a37bb7e
caleb <calebomariba1993@gmail.com>
2024-10-28 18:55:42 +0300

    Add  a WebSocket client to establish a connection with the WebSocket in the course chat room template and be able to send/receive messages

--- courses/admin.py
+++ courses/admin.py
@@ -15,7 +15,7 @@ class ModuleInline(admin.StackedInline):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ['title', 'subject', 'created']
+    list_display = ['title', 'subject', 'created', 'id']
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
     prepopulated_fields = {'slug': ('title',)}
