cf22db0b479d5473f30e9d8cc5729eaafe1fb068
caleb <calebomariba1993@gmail.com>
2024-10-29 00:50:12 +0300

    Implement a functional real-time chat application using Channels where students can chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,14 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.utils import timezone
 
 
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
         # join room group
@@ -25,16 +27,25 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        # send message to WebSocket
+        now = timezone.now()
+        # send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
             'type': 'chat_message',
             'message': message,
+            'user': self.user.username,
+            'datetime': now.isoformat(),
             }
         )
         # receive message from room group
     def chat_message(self, event):
         message = event['message']
+        user = event['user']
+        datetime = event['datetime']
         # send message to WebSocket
-        self.send(text_data=json.dumps({'message': message}))
+        self.send(text_data=json.dumps({
+            'message': message,
+            'user': user,
+            'datetime': datetime,
+            }))
